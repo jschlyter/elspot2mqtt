131eefcd899a62923483e4d7a1fec7f245991044
Jakob Schlyter <jakob@kirei.se>
2022-10-08 15:30:25 +0200

    add support for minima_lookahead

--- elspot2mqtt/costs.py
+++ elspot2mqtt/costs.py
@@ -41,7 +41,13 @@ def to_level(p: float, c: float, levels: List) -> str:
     return res
 
 
-def look_ahead(prices, pm: ExtraCosts, levels: List, avg_window_size: int = 120):
+def look_ahead(
+    prices,
+    pm: ExtraCosts,
+    levels: List,
+    avg_window_size: int = 120,
+    minima_lookahead: int = 4,
+):
     present = time.time() - 3600
     res = []
 
@@ -49,9 +55,9 @@ def look_ahead(prices, pm: ExtraCosts, levels: List, avg_window_size: int = 120)
     total_prices = {t: pm.total_cost(v) for t, v in prices.items()}
     costs = []
 
-    minimas = find_minimas(total_prices)
+    minimas = find_minimas(dataset=spot_prices, minima_lookahead=minima_lookahead)
 
-    for t, cost in total_prices.items():
+    for t, cost in spot_prices.items():
         dt = datetime.fromtimestamp(t).astimezone(tz=None)
 
         costs.append(cost)
--- elspot2mqtt/util.py
+++ elspot2mqtt/util.py
@@ -2,25 +2,21 @@ import time
 from datetime import date, datetime
 from typing import Dict
 
+import pandas as pd
 
-def find_minimas(dataset: Dict[int, float]) -> Dict[int, bool]:
+
+def find_minimas(dataset: Dict[int, float], minima_lookahead: int) -> Dict[int, bool]:
     """Find local minimas in dataset dictionary"""
-    res = {}
-    pairs = [(t, p) for t, p in dataset.items()]
-    l = len(pairs)
-    for i in range(0, l):
-        (t, x) = pairs[i]
-        if i == 0:
-            res[t] = False
-            next
-        elif i == l - 1:
-            res[t] = False
-            next
-        else:
-            (_, a) = pairs[i - 1]
-            (_, b) = pairs[i + 1]
-            res[t] = a > x < b
-    return res
+    pds = pd.Series(dataset, index=dataset.keys())
+    pdf = pd.DataFrame(pds, columns=["cost"])
+    pdf["date"] = pd.to_datetime(pdf.index, unit="s")
+    pdf["min_cost"] = pdf.cost[
+        (pdf.cost.shift(1) > pdf.cost) & (pdf.cost.shift(-1) > pdf.cost)
+    ]
+    pdf["min_ahead"] = pdf["cost"].rolling(window=minima_lookahead).min()
+    pdf["min_ahead"] = pdf["min_ahead"].shift(-minima_lookahead)
+    pdf["minima"] = pdf.min_cost <= pdf.min_ahead
+    return pdf.to_dict()["minima"]
 
 
 def date2timestamp(d: date) -> int:
